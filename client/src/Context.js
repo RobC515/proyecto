b94994f3e4df9be8c3fa868a3836d6bb23fd9494
RobC515 <robertocontreras515@gmail.com>
2022-12-14 10:57:17 -0300

    Update

--- client/src/Context.js
+++ client/src/Context.js
@@ -4,7 +4,7 @@ import Peer from 'simple-peer';
 
 const SocketContext = createContext();
 
-const socket = io('http://localhost:5000');
+const socket = io('https://api.ustream.tk:5555/');
 
 const ContextProvider = ({ children }) => {
   const [callAccepted, setCallAccepted] = useState(false);
